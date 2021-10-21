f802169e140a037515b7259f8d6bfe797b00926b
Nathan Martin <nathan.martin@movista.com>
2021-10-21 14:25:01 -0500

    Include functions for network request tracing
    Firebase Performance does not support automatic collection of Titanium’s network requests.
    
    These functions provide a way for Firebase Performance to get all the info it needs about each network request made, as per
    
    https://firebase.google.com/docs/perf-mon/custom-network-traces?platform=android
    
    The network request (metric) must be identified within the metric HashMap not only with the url string, but also the httpMethod string, in case two network requests are made in row and both of them have the same url (they could have different httpMethods such as HEAD and then POST)
    
    the difference in code is:
    HttpMetric metric = this.metrics.get(url + httpMethod);
    instead of
    HttpMetric metric = this.metrics.get(url);
    
    Example of starting a network request:
    Alloy.Globals.FirebasePerformance.startMetric(url, options.requestType);
                Alloy.Globals.FirebasePerformance.setMetricAttribute(url, options.requestType, 'requestType', options.requestType);
                Alloy.Globals.FirebasePerformance.setMetricAttribute(url, options.requestType, 'url', url);
                Alloy.Globals.FirebasePerformance.setMetricRequestPayloadSize(url, options.requestType, 123);
    
    Example of ending a network request:
    Alloy.Globals.FirebasePerformance.setMetricHttpResponseCode(url, <Ti.Network.HTTPClient>.connectionType, <Ti.Network.HTTPClient>.status);
                Alloy.Globals.FirebasePerformance.setMetricResponseContentType(url, <Ti.Network.HTTPClient>.connectionType, <Ti.Network.HTTPClient>.responseData.mimeType);
                Alloy.Globals.FirebasePerformance.setMetricResponsePayloadSize(url, <Ti.Network.HTTPClient>.connectionType, <Ti.Network.HTTPClient>.responseData.size);
                Alloy.Globals.FirebasePerformance.setMetricAttribute(url, options.requestType, 'responseCode', <Ti.Network.HTTPClient>.status.toString());
                Alloy.Globals.FirebasePerformance.stopMetric(url, <Ti.Network.HTTPClient>.connectionType);

--- android/src/firebase/performance/TitaniumFirebasePerformanceModule.java
+++ android/src/firebase/performance/TitaniumFirebasePerformanceModule.java
@@ -22,6 +22,7 @@ import android.app.Activity;
 
 import com.google.firebase.perf.FirebasePerformance;
 import com.google.firebase.perf.metrics.Trace;
+import com.google.firebase.perf.metrics.HttpMetric;
 
 @Kroll.module(name="TitaniumFirebasePerformance", id="firebase.performance")
 public class TitaniumFirebasePerformanceModule extends KrollModule
@@ -30,6 +31,7 @@ public class TitaniumFirebasePerformanceModule extends KrollModule
 	private static final boolean DBG = TiConfig.LOGD;
 
   private Map<String, Trace> traces = new HashMap<String, Trace>();
+  private Map<String, HttpMetric> metrics = new HashMap<String, HttpMetric>();
 
 	public TitaniumFirebasePerformanceModule()
 	{
@@ -41,6 +43,9 @@ public class TitaniumFirebasePerformanceModule extends KrollModule
   {
     this.traces.clear();
     this.traces = null;
+    
+    this.metrics.clear();
+    this.metrics = null;
   }
 
   @Kroll.method
@@ -61,6 +66,86 @@ public class TitaniumFirebasePerformanceModule extends KrollModule
     Trace trace = this.traces.get(identifier);
     trace.stop();
   }
+  
+  @Kroll.method
+	public void startMetric(String url, String httpMethod) {
+		HttpMetric metric = FirebasePerformance.getInstance().newHttpMetric(url, httpMethod);
+		metric.start();
+
+		this.metrics.put(url + httpMethod, metric);
+	}
+	
+	@Kroll.method
+	public void stopMetric(String url, String httpMethod) {
+		if (this.metrics.get(url + httpMethod) == null) {
+  	
+			Log.e(LCAT, String.format("Cannot find metric for url %s", url + httpMethod));
+			return;
+		}
+
+		HttpMetric metric = this.metrics.get(url + httpMethod);
+		metric.stop();
+	}
+	
+	@Kroll.method
+	public void setMetricRequestPayloadSize(String url, String httpMethod, long bytes) {
+		if (this.metrics.get(url + httpMethod) == null) {
+  	
+			Log.e(LCAT, String.format("Cannot find metric for url %s", url + httpMethod));
+			return;
+		}
+
+		HttpMetric metric = this.metrics.get(url + httpMethod);
+		metric.setRequestPayloadSize(bytes);
+	}
+
+	@Kroll.method
+	public void setMetricHttpResponseCode(String url, String httpMethod, int responseCode) {
+		if (this.metrics.get(url + httpMethod) == null) {
+  	
+			Log.e(LCAT, String.format("Cannot find metric for url %s", url + httpMethod));
+			return;
+		}
+
+		HttpMetric metric = this.metrics.get(url + httpMethod);
+		metric.setHttpResponseCode(responseCode);
+	}
+	
+	@Kroll.method
+	public void setMetricResponseContentType(String url, String httpMethod, String contentType) {
+		if (this.metrics.get(url + httpMethod) == null) {
+  	
+			Log.e(LCAT, String.format("Cannot find metric for url %s", url + httpMethod));
+			return;
+		}
+
+		HttpMetric metric = this.metrics.get(url + httpMethod);
+		metric.setResponseContentType(contentType);
+	}
+	
+	@Kroll.method
+	public void setMetricResponsePayloadSize(String url, String httpMethod, long bytes) {
+		if (this.metrics.get(url + httpMethod) == null) {
+  	
+			Log.e(LCAT, String.format("Cannot find metric for url %s", url + httpMethod));
+			return;
+		}
+
+		HttpMetric metric = this.metrics.get(url + httpMethod);
+		metric.setResponsePayloadSize(bytes);
+	}
+	
+	@Kroll.method
+	public void setMetricAttribute(String url, String httpMethod, String attribute, String value) {
+		if (this.metrics.get(url + httpMethod) == null) {
+  	
+			Log.e(LCAT, String.format("Cannot find metric for url %s", url + httpMethod));
+			return;
+		}
+
+		HttpMetric metric = this.metrics.get(url + httpMethod);
+		metric.putAttribute(attribute, value);
+	}
 
   @Kroll.method
   public void incrementCounter(String identifier, String counterName, Object incrementedBy) {
